a363aa304c931abd35cf4a912d87adb7c8c98527
mikiyasalemayehu-mekonen <mikiyealem@gmail.com>
2026-02-24 00:05:16 +0300

    feat:fix deployment erros

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,7 @@
-
-import arcjet, { createMiddleware, detectBot } from "@arcjet/next";
-import { env } from "./lib/env";
 import { NextRequest, NextResponse } from "next/server";
 import {getSessionCookie} from "better-auth/cookies"
 
-const aj = arcjet({
-  key: env.ARCJET_KEY!, // Get your site key from https://app.arcjet.com
-  rules: [
-    detectBot({
-      mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
-      // Block all bots except the following
-      allow: [
-        "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
-        // Uncomment to allow these other common bot categories
-        // See the full list at https://arcjet.com/bot-list
-        "CATEGORY:MONITOR", // Uptime monitoring services
-        "CATEGORY:PREVIEW", // Link previews e.g. Slack, Discord
-        "STRIPE_WEBHOOK",
-      ],
-    }),
-  ],
-});
-
- async function authmiddleware(request: NextRequest) {
+async function authmiddleware(request: NextRequest) {
     const sessionCookie = await getSessionCookie(request)
 
     if(!sessionCookie) {
@@ -31,17 +10,11 @@ const aj = arcjet({
 
     return NextResponse.next();
 }
+
 export const config = {
-  // matcher tells Next.js which routes to run the middleware on.
-  // This runs the middleware on all routes except for static assets.
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|api/auth).*)"],
+  matcher: ["/admin/:path*"],
 };
 
-// Pass any existing middleware with the optional existingMiddleware prop
-export default createMiddleware(aj,async(request)=>{
-    const path = request.nextUrl.pathname;
-    if(path.startsWith('/admin') ){
-        return authmiddleware(request)
-    }
-    return NextResponse.next();
-});
+export default async function middleware(request: NextRequest) {
+    return authmiddleware(request);
+}
