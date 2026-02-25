176176fa5447c6c72e3901b85fcedb152623ddea
mikiyasalemayehu-mekonen <mikiyealem@gmail.com>
2026-02-26 00:52:43 +0300

    fix:fixed primsa issues

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,23 @@
+/*eslint-disable @typescript-eslint/ban-ts-comment*/
+//@ts-ignore
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+import type { NextConfig } from "next";
 
-
-const nextConfig = {
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '*.t3.storage.dev', // Wildcard for all subdomains
+        hostname: '*.t3.storage.dev',
         pathname: '/**',
       },
     ],
+  },
+  webpack: (config: any, { isServer }: { isServer: boolean }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+    return config
   },
 };
 
